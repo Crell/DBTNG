80bccf2e323f8965fa4e5021783dd40dc7fcb95d
Joshua Waihi <josh@catalyst.net.nz>
2011-03-09 16:24:20 -0600

    added a "default.settings.php" and made schema.test work!

---
+++ default.settings.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * Database settings:
+ *
+ * The $databases array specifies the database connection or
+ * connections that DBTNG may use. DBTNG is able to connect
+ * to multiple databases, including multiple types of databases,
+ * during the same request.
+ *
+ * Each database connection is specified as an array of settings,
+ * similar to the following:
+ * @code
+ * array(
+ *   'driver' => 'mysql',
+ *   'database' => 'databasename',
+ *   'username' => 'username',
+ *   'password' => 'password',
+ *   'host' => 'localhost',
+ *   'port' => 3306,
+ *   'prefix' => 'myprefix_',
+ *   'collation' => 'utf8_general_ci',
+ * );
+ * @endcode
+ *
+ * The "driver" property indicates what DBTNG driver the connection
+ * should use.  This is usually the same as the name of the database
+ * type, such as mysql or sqlite, but not always.  The other
+ * properties will vary depending on the driver.  For SQLite, you must
+ * specify a database file name in a directory that is writable by the
+ * webserver.  For most other drivers, you must specify a
+ * username, password, host, and database name.
+ *
+ * Some database engines support transactions.  In order to enable
+ * transaction support for a given database, set the 'transactions' key
+ * to TRUE.  To disable it, set it to FALSE.  Note that the default value
+ * varies by driver.  For MySQL, the default is FALSE since MyISAM tables
+ * do not support transactions.
+ *
+ * For each database, you may optionally specify multiple "target" databases.
+ * A target database allows DBTNG to try to send certain queries to a
+ * different database if it can but fall back to the default connection if not.
+ * That is useful for master/slave replication, as DBTNG may try to connect
+ * to a slave server when appropriate and if one is not available will simply
+ * fall back to the single master server.
+ *
+ * The general format for the $databases array is as follows:
+ * @code
+ * $databases['default']['default'] = $info_array;
+ * $databases['default']['slave'][] = $info_array;
+ * $databases['default']['slave'][] = $info_array;
+ * $databases['extra']['default'] = $info_array;
+ * @endcode
+ *
+ * In the above example, $info_array is an array of settings described above.
+ * The first line sets a "default" database that has one master database
+ * (the second level default). The second and third lines create an array
+ * of potential slave databases. DBTNG will select one at random for a given
+ * request as needed. The fourth line creates a new database with a name of
+ * "extra".
+ *
+ * For a single database configuration, the following is sufficient:
+ * @code
+ * $databases['default']['default'] = array(
+ *   'driver' => 'mysql',
+ *   'database' => 'databasename',
+ *   'username' => 'username',
+ *   'password' => 'password',
+ *   'host' => 'localhost',
+ *   'prefix' => 'main_',
+ *   'collation' => 'utf8_general_ci',
+ * );
+ * @endcode
+ *
+ * You can optionally set prefixes for some or all database table names
+ * by using the 'prefix' setting. If a prefix is specified, the table
+ * name will be prepended with its value. Be sure to use valid database
+ * characters only, usually alphanumeric and underscore. If no prefixes
+ * are desired, leave it as an empty string ''.
+ *
+ * To have all database names prefixed, set 'prefix' as a string:
+ * @code
+ *   'prefix' => 'main_',
+ * @endcode
+ * To provide prefixes for specific tables, set 'prefix' as an array.
+ * The array's keys are the table names and the values are the prefixes.
+ * The 'default' element is mandatory and holds the prefix for any tables
+ * not specified elsewhere in the array. Example:
+ * @code
+ *   'prefix' => array(
+ *     'default'   => 'main_',
+ *     'users'     => 'shared_',
+ *     'sessions'  => 'shared_',
+ *     'role'      => 'shared_',
+ *     'authmap'   => 'shared_',
+ *   ),
+ * @endcode
+ * You can also use a reference to a schema/database as a prefix. This maybe
+ * useful if your DBTNG installation exists in a schema that is not the default
+ * or you want to access several databases from the same code base at the same
+ * time.
+ * Example:
+ * @code
+ *   'prefix' => array(
+ *     'default'   => 'main.',
+ *     'users'     => 'shared.',
+ *     'sessions'  => 'shared.',
+ *     'role'      => 'shared.',
+ *     'authmap'   => 'shared.',
+ *   );
+ * @endcode
+ * NOTE: MySQL and SQLite's definition of a schema is a database.
+ *
+ * Database configuration format:
+ * @code
+ *   $databases['default']['default'] = array(
+ *     'driver' => 'mysql',
+ *     'database' => 'databasename',
+ *     'username' => 'username',
+ *     'password' => 'password',
+ *     'host' => 'localhost',
+ *     'prefix' => '',
+ *   );
+ *   $databases['default']['default'] = array(
+ *     'driver' => 'pgsql',
+ *     'database' => 'databasename',
+ *     'username' => 'username',
+ *     'password' => 'password',
+ *     'host' => 'localhost',
+ *     'prefix' => '',
+ *   );
+ *   $databases['default']['default'] = array(
+ *     'driver' => 'sqlite',
+ *     'database' => '/path/to/databasefilename',
+ *   );
+ * @endcode
+ */
+
+global $databases;
+
+$databases['default']['default'] = array(
+  'driver' => 'pgsql',
+  'database' => 'dbtng',
+  'username' => 'drupal',
+  'host' => 'localhost',
+  'prefix' => '',
+);
+
+if (!function_exists('t')) {
+  function t($text, $vars = array()) {
+    return strtr($text, $vars);
+  }
+}
+
+define('DBTNG_DIR', dirname(__FILE__));
+
+include_once DBTNG_DIR . '/database.inc';
--- tests/DBTNG_PHPUnit_Framework_TestCase.php
+++ tests/DBTNG_PHPUnit_Framework_TestCase.php
@@ -2,9 +2,39 @@
 
 class DBTNG_PHPUnit_Framework_TestCase extends PHPUnit_Framework_TestCase {
 
+  protected $databasePrefix;
+
   public function __construct() {
+    parent::__construct();
     $settings = realpath(dirname(__FILE__) . '/../settings.php');
     require_once $settings;
+  }
+
+  public function setUp() {
+    $this->databasePrefix = 'dbtngtest' . mt_rand(1000, 1000000);
+    // Clone the current connection and replace the current prefix.
+    $connection_info = Database::getConnectionInfo('default');
+    Database::renameConnection('default', 'dbtng_original_default');
+    foreach ($connection_info as $target => $value) {
+      $connection_info[$target]['prefix'] = array(
+        'default' => $value['prefix']['default'] . $this->databasePrefix,
+      );
+    }
+    Database::addConnectionInfo('default', 'default', $connection_info['default']);
+  }
+
+  public function tearDown() {
+    // This would be better if it were database specific, but for now we'll
+    // live with information_schema.
+    $connection_info = Database::getConnectionInfo('default');
+    $result = db_query("SELECT table_name FROM information_schema.tables WHERE table_catalog = '" . $connection_info['database'] . "' AND table_schema = 'public'")->fetchAll();
+    foreach ($result as $row) {
+      db_drop_table($row->table_name);
+    }
+
+    // Get back to the original connection.
+    Database::removeConnection('default');
+    Database::renameConnection('dbtng_original_default', 'default');
   }
 
 }
